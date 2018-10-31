07d518ba11ac5d2c721398b11acf5073c845794d
andreas <andreas.peter.ch@gmail.com>
2018-10-31 16:15:49 +0000

    Fix Z2 symmetric peps-tensor and environment

--- src/ising.jl
+++ src/ising.jl
@@ -9,7 +9,7 @@ function magofβ(β)
 end
 
 #Crone Thesis tensors:
-function atens(β)
+function isingtensor(β)
     a = zeros(Float64,2,2,2,2)
     a[1,1,1,1] = a[2,2,2,2] = 1
     cβ = sqrt(cosh(β))
@@ -19,7 +19,7 @@ function atens(β)
     return DTensor(a)
 end
 
-function asztens(β)
+function isingmagtensor(β)
     a = zeros(Float64,2,2,2,2)
     a[1,1,1,1] = 1
     a[2,2,2,2] = -1
@@ -49,98 +49,85 @@ function isingenvironment(β, χ; fixed::Bool = true)
     return (DTensor(C), DTensor(T))
 end
 
-function isingenvironmentz2(β, χ; fixed::Bool = true)
+function isingenvironmentz2(β::Float64, χ::Int)
     C = ZNTensor{Float64,2,2}(([χ,χ],[χ,χ]),(-1,-1))
     T = ZNTensor{Float64,3,2}(([χ,χ],[1,1],[χ,χ]),(1,-1,1))
-    C[(0,0)] = zeros(Float64,χ,χ)
-    T[(0,0,0)] = zeros(Float64,χ,1,χ)
-    C[(0,0)][1,1] = 1
-    T[(0,0,0)][1,1,1] = 1
-    if !fixed
-        C[(1,1)] = zeros(Float64,χ,χ)
-        T[(1,1,1)] = zeros(Float64,χ,1,χ)
-        C[(1,1)][1,1] = 1
-        T[(1,1,1)][1,1,1] = 1
+    for i in Iterators.filter(iseven ∘ sum, Iterators.product(charges(C)...))
+        C[i] = zeros(Float64,χ,χ)
+        C[i][1] = 1
+    end
+    for i in Iterators.filter(iseven ∘ sum, Iterators.product(charges(T)...))
+        T[i] = zeros(Float64,χ,1,χ)
+        T[i][1] = 1
     end
     q = ZNTensor{Float64,2,2}((0:1,0:1),([1,1],[1,1]),(1,-1))
     qχ = ZNTensor{Float64,2,2}((0:1,0:1),([χ,χ],[χ,χ]),(1,-1))
-    cβ = sqrt(cosh(β))
-    sβ = sqrt(sinh(β))
-    t00 = 2^(-1/2) * (cβ + sβ)
-    t01 = 2^(-1/2) * (cβ - sβ)
-    q[(0,0)] = reshape([t00],1,1)
-    q[(1,1)] = reshape([t00],1,1)
-    q[(0,1)] = reshape([t01],1,1)
-    q[(1,0)] = reshape([t01],1,1)
+    cβ = [sqrt(cosh(β))]
+    sβ = [sqrt(sinh(β))]
+    q[(0,0)] = reshape(sqrt(2) * cβ, 1,1)
+    q[(1,1)] = reshape(sqrt(2) * cβ, 1,1)
     qχ[(0,0)] = zeros(Float64,χ,χ)
     qχ[(1,1)] = zeros(Float64,χ,χ)
-    qχ[(0,1)] = zeros(Float64,χ,χ)
-    qχ[(1,0)] = zeros(Float64,χ,χ)
-    qχ[(0,0)][1,1] = t00
-    qχ[(1,1)][1,1] = t00
-    qχ[(0,1)][1,1] = t01
-    qχ[(1,0)][1,1] = t01
+    qχ[(0,0)][1] = sqrt(2) * cβ[1]
+    qχ[(1,1)][1] = sqrt(2) * sβ[1]
     @tensor T[1,2,3] := T[-1,-2,-3] * qχ[1,-1] * q[2,-2] * qχ[3,-3]
     @tensor C[1,2] := C[-1,-2] * qχ[1,-1] * qχ[2,-2]
     return (C, T)
 end
 
-function atensz2(β)
+function isingtensorz2(β)
     a = ZNTensor{Float64,4,2}((0:1,0:1,0:1,0:1),([1,1],[1,1],[1,1],[1,1]), (1,1,1,1))
+    for i in Iterators.filter(iseven ∘ sum, Iterators.product(charges(a)...))
+        a[i] = reshape([1],1,1,1,1)
+    end
     b = ZNTensor{Float64,2,2}((0:1,0:1),([1,1],[1,1]),(1,-1))
-    cβ = reshape([sqrt(cosh(β))],1,1)
-    sβ = reshape([sqrt(sinh(β))],1,1)
-    a[(0,0,0,0)] = reshape([1],1,1,1,1)
-    a[(1,1,1,1)] = reshape([1],1,1,1,1)
-    b[(0,0)] = 2^(-1/2) * (cβ + sβ)
-    b[(1,1)] = 2^(-1/2) * (cβ + sβ)
-    b[(0,1)] = 2^(-1/2) * (cβ - sβ)
-    b[(1,0)] = 2^(-1/2) * (cβ - sβ)
+    b[(0,0)] = sqrt(2) * reshape([sqrt(cosh(β))],1,1)
+    b[(1,1)] = sqrt(2) * reshape([sqrt(sinh(β))],1,1)
     @tensor a[o1,o2,o3,o4] := a[c1,c2,c3,c4] * b[o1,c1] * b[o2,c2] * b[o3,c3] * b[o4,c4]
     return a
 end
 
-function asztensz2(β)
+function isingmagtensorz2(β)
     a = ZNTensor{Float64,4,2}((0:1,0:1,0:1,0:1),([1,1],[1,1],[1,1],[1,1]), (1,1,1,1))
+    for i in Iterators.filter(iseven ∘ sum, Iterators.product(0:1,0:1,0:1,0:1))
+        a[i] = reshape([1],1,1,1,1)
+    end
     b = ZNTensor{Float64,2,2}((0:1,0:1),([1,1],[1,1]),(1,-1))
-    cβ = reshape([sqrt(cosh(β))],1,1)
-    sβ = reshape([sqrt(sinh(β))],1,1)
-    a[(0,0,0,0)] = reshape([1],1,1,1,1)
-    a[(1,1,1,1)] = reshape([-1],1,1,1,1)
-    b[(0,0)] = 2^(-1/2) * (cβ + sβ)
-    b[(1,1)] = 2^(-1/2) * (cβ + sβ)
-    b[(0,1)] = 2^(-1/2) * (cβ - sβ)
-    b[(1,0)] = 2^(-1/2) * (cβ - sβ)
+    b[(0,0)] = sqrt(2) * reshape([sqrt(cosh(β))],1,1)
+    b[(1,1)] = sqrt(2) * reshape([sqrt(sinh(β))],1,1)
     @tensor a[o1,o2,o3,o4] := a[c1,c2,c3,c4] * b[o1,c1] * b[o2,c2] * b[o3,c3] * b[o4,c4]
     return a
 end
 
-atenses(β) = (atens(β), asztens(β))
-atensesz2(β) = (atensz2(β), asztensz2(β))
+isingtensors(β) = (isingtensor(β), isingmagtensor(β))
+isingtensorsz2(β) = (isingtensorz2(β), isingmagtensorz2(β))
 
 # isingpart(β) = partitionfun(ising, β)
 
 magnetisation(state::rotsymCTMState, a, asz) = magnetisation(state.C, state.T, a, asz)
 magnetisation((C,T,a,asz)) = magnetisation(C,T,a,asz)
 
-function magnetisation(C::AbstractTensor{S,2}, T::AbstractTensor{S,3}, a::AbstractTensor{S,4}, asz::AbstractTensor{S,4}) where S
+function magnetisation(C::DTensor{S,2}, T::DTensor{S,3}, a::DTensor{S,4}, asz::DTensor{S,4}) where S
     @tensor ctc[1,2,3] := C[1,-1] * T[-1,2,-2] * C[-2,3]
     @tensor begin
         mag[]  := ctc[-1,-2,-3] * T[-3,-4,-5] * ctc[-7,-6,-5] * T[-7,-8,-1] * asz[-2,-4,-6,-8]
         norm[] := ctc[-1,-2,-3] * T[-3,-4,-5] * ctc[-5,-6,-7] * T[-7,-8,-1] * a[-2,-4,-6,-8]
-        # norm[] := C[-1,-2] * C[-2,-3] * C[-3,-4] * C[-4,-1]
     end
     return scalar(mag)/scalar(norm)
 end
 
+function magnetisation(C::ZNTensor{S,2}, T::ZNTensor{S,3}, a::ZNTensor{S,4}, asz::ZNTensor{S,4}) where S
+    return 0
+end
+
 function isingctm(β, χ, fixed::Bool = true; kwargs...)
     cinit, tinit = isingenvironment(β, χ, fixed = fixed)
-    a, asz = atenses(β)
+    a, asz = isingtensors(β)
     (ctm(a, asz, χ; kwargs...)[2]..., a, asz)
 end
 
-function isingctmz2(β, χ, fixed::Bool = true; kwargs...)
-    cinit, tinit = isingenvironmentz2(β, χ, fixed = fixed)
-    a, asz = atensesz2(β)
+function isingctmz2(β, χ; kwargs...)
+    cinit, tinit = isingenvironmentz2(β, χ)
+    a, asz = isingtensorsz2(β)
     (ctm(a, asz, χ; kwargs...)[2]..., a, asz)
 end
