12d5634ab646d588c454d629a1ca39b9ddcd6221
Andreas Peter <andreas.peter.ch@gmail.com>
2019-03-25 12:12:19 +0100

    Add tests to check that all iterators result in same for 2d classic ising

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, CornerTransferMethods, TensorOperations, TensorNetworkTensors
+using Test, CornerTransferMethods, TensorOperations, TensorNetworkTensors, LinearAlgebra
 @testset "CornerTransferMethods" begin
     @testset "tfisingmpo" begin
         β = randn()
@@ -37,6 +37,14 @@ using Test, CornerTransferMethods, TensorOperations, TensorNetworkTensors
             res = toarray(fuselegs(op2,((1,2,3),(4,5,6)))[1])
             @test ref ≈ refe && ref ≈ res
         end
+        op = tfisingpropagator(1e-5)
+        @tensor op2[1,2,3,4,5,6] := op[1,-2,4,-1] * op[2,-3,5,-2] * op[3,-1,6,-3];
+        prop = toarray(fuselegs(op2,((1,2,3),(4,5,6)))[1])
+        ham = -(kron(σz,id,id) + kron(id,σz,id) + kron(id,id,σz) +
+                kron(σx,σx,id) + kron(σx,id,σx) + kron(id,σx,σx))
+        λp, vecsp = eigen(prop)
+        λh, vecsh = eigen(ham)
+        @test vecsp[:,argmax(λp)] ≈ vecsh[:,argmin(λh)]
 
         @test kron(-σx,σx) + kron(id,-σz) + kron(-σz,id) ≈ toarray(fuselegs(tfisinghamiltonian(true),((1,2),(3,4)))[1])
 
@@ -47,5 +55,23 @@ using Test, CornerTransferMethods, TensorOperations, TensorNetworkTensors
         sth∞ = tfisingctm(1e-3, 8, 1e5, maxit=10^4, period = 10^2, verbose=false)
         @test mag(sth∞) ≈ 1
         @test energy(sth∞) ≈ -1
+    end
+
+    @testset "algorithms equality" begin
+        A = CornerTransferMethods.isingtensor(CornerTransferMethods.βc)
+        χ = 4
+        rotsymiter = rotsymctmiterable(A, χ)
+        tciter = transconjctmiterable(A, χ)
+        iter = ctmiterable(A, χ)
+        rotsymstate = ctm_kernel(rotsymiter, maxit=10^6, verbose=false)
+        tcstate = ctm_kernel(tciter, maxit=10^6, verbose=false)
+        state = ctm_kernel(iter, maxit=10^6, verbose=false)
+        eoes = cornereoe.([state.Cs[1], state.Cs[2], state.Cs[3], state.Cs[4],
+                           rotsymstate.C, tcstate.C])
+        @test all((x ≈ y for x in eoes, y in eoes))
+
+        cls = clength.([state.Ts[1], state.Ts[2], state.Ts[3], state.Ts[4],
+                        rotsymstate.T, tcstate.Ts[1], tcstate.Ts[2]])
+        @test all((x ≈ y for x in cls, y in cls))
     end
 end
